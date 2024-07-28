013a512d8eeaf3d8fe08757aaba1512562a24326
Aditya Mistri <123372823+aditya-mistri@users.noreply.github.com>
2024-07-28 21:53:06 +0530

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://propertyyhuntt.onrender.com",
+  baseURL: "https://propertyyhuntt.onrender.com/api",
   withCredentials: true,
 });
 
